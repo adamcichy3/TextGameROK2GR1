df63ee34f38d6fa9ad5fa97f6f30d44c7dee472a
Adam Cichy <adam.cichyy3@gmail.com>
2023-03-15 17:50:29 +0100

    Added new project with textgame script

---
+++ TextGameROK2GR1/Assets/Scripts/TextGame.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    // Program zgaduje liczbe ktora wymyslilismy sobie z konkretnego przedzialu liczbowego.
+    // Mamy przedzial od 1 do 1000 i program pyta czy wymyslona liczba jest wieksza lub mniejsza lub rowna  - np. 328
+    // No i uzytkownik odpowiada wieksza lud mniejsza lub rowna
+    // ALGORYTM -> Wyszukiwanie binarnie -> losowo
+
+    // 500
+    // 328
+    // mniejsza
+    // min = 1 max = 500
+    // (min + max) /2  = 501/2= 250
+
+    int min = 1;
+    int max = 1000;
+    int shoot = 500;
+
+    void Start()
+    {
+        // typ nazwe i ew. podac poczatkowa wartosc
+       
+
+        Debug.Log("Witam w grze - pomysl w glowie liczbe od 1 do 1000");
+        Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od 500?");
+        // print string
+        // Nacisnij gorna strzalke aby odpowiedziec ze wieksza
+       
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = shoot;
+            shoot = (min + max) / 2; // 250
+            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od "+shoot + " ?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = shoot;
+            shoot = (min + max) / 2;
+            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + " ?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("KOMPUTER ZGADL LICZBE");
+        }
+    }
+}
